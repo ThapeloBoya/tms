a34c8cae327870218e53e20d7566c2cf543c8b1a
Thapelo B <88501835+ThapeloBoya@users.noreply.github.com>
2025-09-19 08:18:03 +0200

    Add files via upload

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; // your backend URL
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/auth`;
 
 const AuthContext = createContext();
 
